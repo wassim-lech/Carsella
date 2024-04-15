626923bf326da576c2c01185b1394ecf3cb6ef49
wassim <wassimlecheb@gmail.com>
2024-04-15 22:33:30 +0100

    menu

---
+++ CarList.js
@@ -0,0 +1,47 @@
+const slides = document.querySelector(".slides");
+const slideImages = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let visibleSlides = 5;
+let intervalId = null;
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider() {
+  if (slideImages.length > 0) {
+    showSlides(slideIndex);
+    intervalId = setInterval(nextSlide, 5000);
+  }
+}
+
+function showSlides(index) {
+  const start = index * visibleSlides;
+  const end = start + visibleSlides;
+
+  slideImages.forEach((img, i) => {
+    if (i >= start && i < end) {
+      img.classList.add("show");
+    } else {
+      img.classList.remove("show");
+    }
+  });
+}
+
+function prevSlide() {
+  clearInterval(intervalId);
+  slideIndex--;
+  if (slideIndex < 0) {
+    slideIndex = Math.ceil(slideImages.length / visibleSlides) - 1;
+  }
+  showSlides(slideIndex);
+  intervalId = setInterval(nextSlide, 5000);
+}
+
+function nextSlide() {
+  clearInterval(intervalId);
+  slideIndex++;
+  if (slideIndex >= Math.ceil(slideImages.length / visibleSlides)) {
+    slideIndex = 0;
+  }
+  showSlides(slideIndex);
+  intervalId = setInterval(nextSlide, 5000);
+}
